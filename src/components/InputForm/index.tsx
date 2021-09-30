edbbdacd32dfc96a024fa6f121d82016f7179c44
Rômulo <romfe89@gmail.com>
2021-09-30 13:59:29 -0300

    Input form created

--- src/components/InputForm/index.tsx
+++ src/components/InputForm/index.tsx
@@ -1,13 +1,39 @@
 import { Container } from './styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+
+
 export function InputForm() {
+  //const [gender, setGender] = useState('');
+  const recordHandle = () => {
+    //record logic
+  }
   return (
     <Container>
       <TextField
         id="name-input"
         label="Name"
-        value="Name"
       />
+      <TextField
+        id="age-input"
+        label="Age"
+        type="number"
+      />
+      <TextField
+        id="history-input"
+        label="Patient History"
+        multiline
+      />
+      <TextField
+        id="gender-input"
+        label="Patient Gender"
+      />
+      <Button
+        variant="contained"
+        onClick={recordHandle}
+      >Record</Button>
+
     </Container>
   );
 }
---
+++ src/components/PatientRecord/index.tsx
@@ -0,0 +1,8 @@
+import { Container } from './styles';
+export function PatientRecord() {
+  return (
+    <Container>
+
+    </Container>
+  );
+}
